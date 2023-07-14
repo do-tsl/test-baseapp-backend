f520fbe108c0ecb70f5dc04b70c66a735ff3a3ee
Daniel <do@tsl.io>
2023-07-14 09:06:25 -0300

    test release

--- baseapp-reactions/setup.py
+++ baseapp-reactions/setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="baseapp-reactions",  # How you named your package folder (MyLib)
-    packages=["baseapp-reactions"],  # Chose the same as "name"
-    version="0.7",  # Start with a small number and increase it with every change you make
-
-    install_requires=[  # I get to this in a second
-        "validators",
-        "beautifulsoup4",
-    ],
+    name="baseapp-reactions",
+    packages=["baseapp-reactions"],
+    version="0.7",
 )
